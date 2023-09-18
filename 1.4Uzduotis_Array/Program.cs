a4e5bfde0d888ebcae971ee19647ca6eff1870d2
Laura <Laura@DESKTOP-BESSB6E>
2023-09-18 22:57:51 +0300

    Add project files.

---
+++ 1.4Uzduotis_Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace _1._4Uzduotis_Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] ints = { 1, 2, 3, 4, 5 };
+
+            ReverseInts(ints);
+        }
+        public static void ReverseInts(int[] ints)
+        {
+            for (int i = ints.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(ints[i]);
+            }
+        }
+    }
+}
